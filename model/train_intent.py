306aedde0d17b55e831916be1b0e3f3c9f48c02a
aditya42as <adityasingh42.work@gmail.com>
2026-03-19 21:51:48 +0530

    Updated model path and validation of data

--- model/train_intent.py
+++ model/train_intent.py
@@ -4,6 +4,7 @@ import joblib
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import Trainer, TrainingArguments
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
 from config.settings import MODEL_NAME, MODEL_SAVE_PATH
 
@@ -12,7 +13,11 @@ class IntentDataset(Dataset):
 
     def __init__(self, texts, labels, tokenizer):
 
-        self.encodings = tokenizer(texts, truncation=True, padding=True)
+        self.encodings = tokenizer(
+            texts,
+            truncation=True,
+            padding=True
+        )
 
         self.labels = labels
 
@@ -31,55 +36,90 @@ class IntentDataset(Dataset):
 
 def train():
 
+    print("Loading dataset...")
+
     df = pd.read_csv("data/intents_dataset.csv")
 
     texts = df["text"].tolist()
-
     labels = df["intent"].tolist()
 
-    encoder = LabelEncoder()
+    print("Total samples:", len(texts))
 
-    encoded_labels = encoder.fit_transform(labels)
+
+
+    encoder = LabelEncoder()
+    encoder.fit(labels)
+
+    encoded_labels = encoder.transform(labels)
+
+
+
+    train_texts, val_texts, train_labels, val_labels = train_test_split(
+        texts,
+        encoded_labels,
+        test_size=0.2,
+        random_state=42
+    )
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-    dataset = IntentDataset(texts, encoded_labels, tokenizer)
+
+   
+
+    train_dataset = IntentDataset(train_texts, train_labels, tokenizer)
+    val_dataset = IntentDataset(val_texts, val_labels, tokenizer)
+
+
+
 
     model = AutoModelForSequenceClassification.from_pretrained(
         MODEL_NAME,
         num_labels=len(set(encoded_labels))
     )
 
+
     training_args = TrainingArguments(
 
         output_dir=MODEL_SAVE_PATH,
 
-        num_train_epochs=3,
+        num_train_epochs=5,
 
         per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
 
-        logging_steps=10
+        eval_strategy="epoch",
+        save_strategy="epoch",
 
+        logging_steps=10,
+
+        load_best_model_at_end=True
     )
 
     trainer = Trainer(
 
         model=model,
-
         args=training_args,
 
-        train_dataset=dataset
-
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset
     )
 
+
+    print("Training started...")
+
     trainer.train()
+
+
+
+    print("Saving model...")
 
     model.save_pretrained(MODEL_SAVE_PATH)
     tokenizer.save_pretrained(MODEL_SAVE_PATH)
 
     joblib.dump(encoder, MODEL_SAVE_PATH + "/label_encoder.pkl")
 
+    print("Training complete. Model saved to:", MODEL_SAVE_PATH)
+
 
 if __name__ == "__main__":
-
     train()
